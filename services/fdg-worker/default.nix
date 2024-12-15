229557d1841baa97933f06514a00fb19a5c9e503
Ember 'n0emis' Keske <git@n0emis.eu>
2024-12-15 18:05:19 +0100

    add ris api secrets

--- services/fdg-worker/default.nix
+++ services/fdg-worker/default.nix
@@ -4,9 +4,16 @@
     worker = {
       enable = true;
     };
-    settings.database = {
-      host = "martian.wg.infra.fahrplandatengarten.de";
-      password = "$DB_PASSWORD";
+    settings = {
+      database = {
+        host = "martian.wg.infra.fahrplandatengarten.de";
+        password = "$DB_PASSWORD";
+      };
+      "ris.stations" = {
+        url = "$RIS_STATIONS_URL";
+        client_id = "$RIS_STATIONS_ID";
+        api_key = "$RIS_STATIONS_KEY";
+      };
     };
     environmentFile = config.sops.secrets."services/fdg-app/env".path;
   };
